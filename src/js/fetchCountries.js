8fcac6ce864e26e6152e757c6366bd7d1c0f5d24
Tanya Sabadyshyna <sabadyshyna@gmail.com>
2021-11-01 18:40:56 +0200

    edit rest api back

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://restcountries.com/rest/v2/name';
+const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
 function fetchCountries(countryName) {
   return fetch(`${BASE_URL}/${countryName}`).then(response => response.json());
